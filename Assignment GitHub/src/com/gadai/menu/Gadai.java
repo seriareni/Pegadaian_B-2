dd70109635f074f58248c0a3debd521b12d39851
rizzaff <rizza.tees@gmail.com>
2019-11-06 00:28:18 +0700

    update class gadai
    add getter and setter for class gadai

--- Assignment GitHub/src/com/gadai/menu/Gadai.java
+++ Assignment GitHub/src/com/gadai/menu/Gadai.java
@@ -28,6 +28,86 @@ public class Gadai {
 		this.price = price;
 		this.stringLength = stringLength;
 	}
-	
+
+	public Scanner getInput() {
+		return input;
+	}
+
+	public void setInput(Scanner input) {
+		this.input = input;
+	}
+
+	public int getCountID() {
+		return countID;
+	}
+
+	public void setCountID(int countID) {
+		this.countID = countID;
+	}
+
+	public int getItemCount() {
+		return itemCount;
+	}
+
+	public void setItemCount(int itemCount) {
+		this.itemCount = itemCount;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getBiaya() {
+		return biaya;
+	}
+
+	public void setBiaya(double biaya) {
+		this.biaya = biaya;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getProduct() {
+		return product;
+	}
+
+	public void setProduct(String product) {
+		this.product = product;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getStringLength() {
+		return stringLength;
+	}
+
+	public void setStringLength(int stringLength) {
+		this.stringLength = stringLength;
+	}
+
 	
 }
