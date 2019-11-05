4287afe9839fa5b9f6daf48bbc2fd95ccb5989c2
hajidito <hajidito.kerja@gmail.com>
2019-11-06 00:24:30 +0700

    inisiasi inventory tebus
    membuat struktur data untuk kebutuhan menu tebus

--- Assignment GitHub/src/com/gadai/data/Inventory.java
+++ Assignment GitHub/src/com/gadai/data/Inventory.java
@@ -1,17 +1,49 @@
 package com.gadai.data;
 
 public class Inventory {
+	int id;
 	String name;
 	String productCategory;
 	String description;
+	String status;
 	double price;
+	double utang;
 	
-	public Inventory(String name, String productCategory, String description, double price) {
+	public Inventory(int id, String name, String productCategory, String description, String status, double price,
+			double utang) {
 		super();
+		this.id = id;
 		this.name = name;
 		this.productCategory = productCategory;
 		this.description = description;
+		this.status = status;
 		this.price = price;
+		this.utang = utang;
+	}
+	
+	public static void printHeaderTebus() {
+        String leftAlignFormat = "| %-15s | %-8s | %-10s | %-9s | %-15s | %n";
+
+        printLine();
+        System.out.format(leftAlignFormat, "ID", "Product", "Price", "Status", "Utang");
+        printLine();
+    }
+	
+    public static void printLine() {
+        System.out.format("---------------------------------------------------%n");
+    }
+
+    public void printRowTebus() {
+        String leftAlignFormat = "| %-15s | %-8s | %.2f | %-9s |  %.2f | %n";
+        System.out.format(leftAlignFormat, id, productCategory, price, status, utang);
+    }
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public String getName() {
@@ -38,6 +70,14 @@ public class Inventory {
 		this.description = description;
 	}
 
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
 	public double getPrice() {
 		return price;
 	}
@@ -45,6 +85,12 @@ public class Inventory {
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
-	
+
+	public double getUtang() {
+		return utang;
+	}
+
+	public void setUtang(double utang) {
+		this.utang = utang;
+	}
 }
