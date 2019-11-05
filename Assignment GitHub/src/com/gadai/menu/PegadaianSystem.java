d7dfd3c15eaf70a36e440bb9e6370631bef815f5
Seria Reni Dwi Astiwi <seriareny.da@gmail.com>
2019-11-06 00:30:59 +0700

    menampilkan data tebus
    membuat tabel data barang yang akan ditebus

--- Assignment GitHub/src/com/gadai/menu/PegadaianSystem.java
+++ Assignment GitHub/src/com/gadai/menu/PegadaianSystem.java
@@ -1,41 +1,55 @@
 package com.gadai.menu;
 import java.util.*;
 
+import com.gadai.data.*;
+
 public class PegadaianSystem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		int menuNumber;
 		Scanner input = new Scanner(System.in);
+		ArrayList<Inventory> newInventory = new ArrayList<>();
+		int countID=0;
 
-		System.out.println("+-------------------------------+");
-		System.out.println("      		MENU			");
-		System.out.println("+-------------------------------+");
-		System.out.println("1. Gadai");
-		System.out.println("2. Tebus");
-		System.out.println("3. Tampilkan Inventory");
-		System.out.println("4. Exit");
-		System.out.println("+-------------------------------+");
-
-		do {					
+		int itemCount = newInventory.size();
+		int id =0;
+		double biaya=0;
+		String name="";
+		String product="";
+		String description="";
+		double price=0;
+		int stringLength = 16;
+		
+//		Gadai data = new Gadai(countID, itemCount, id, biaya, name, product, description, price, stringLength);
+		
+		do {
+			System.out.println("+-------------------------------+");
+			System.out.println("      		MENU			");
+			System.out.println("+-------------------------------+");
+			System.out.println("1. Gadai");
+			System.out.println("2. Tebus");
+			System.out.println("3. Tampilkan Inventory");
+			System.out.println("4. Exit");
+			System.out.println("+-------------------------------+");
 			System.out.print("Masukkan Menu Pilihan : ");
 			menuNumber = input.nextInt();
 
 			if(menuNumber==1) {
-				System.out.println("Menu Gadai");
-			} else if(menuNumber==2) {
-				System.out.println("Menu Tebus");
+			} 
+			else if(menuNumber==2) {
+				Inventory.printHeaderTebus();
+				for (Inventory inventory : newInventory) inventory.printRowTebus();
+				Inventory.printLine();
+				
 			} else if(menuNumber==3) {
-				System.out.println("Tampilkan Inventory");
-			} else if(menuNumber==0) {
+			} else if(menuNumber==4) {
 				System.out.println("Exit");
 			} else {
 				System.out.println("Menu Tidak Ada");
 			}
-		} while(menuNumber!=0);
+		} while(menuNumber!=4);
 
 	}
 }
-
-
